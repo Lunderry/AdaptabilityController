50ee8c309c1e8d2b41a7dc1a28e745328eff839d
Lunderry <115415867+Lunderry@users.noreply.github.com>
2024-07-21 20:50:26 -0600

    Update: Create Adaptability Controller

--- src/init.lua
+++ src/init.lua
@@ -1,85 +1,108 @@
---!nonstrict
+--!strict
 local ContextActionService = game:GetService("ContextActionService")
-local Players = game:GetService("Players")
+local HttpService = game:GetService("HttpService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 --
-local Module = ReplicatedStorage.Module
-local SubModule = Module.SubModule
+local lib = script.lib
 --
-local ConnectionMeta = require(SubModule.ConnectionMeta)
+local ConnectionMeta = require(lib.ConnectionMeta)
 --
-local plr = Players.LocalPlayer or Players.PlayerAdded:Wait()
---
-local folderKeyCode = plr:WaitForChild("FolderKeyCode")
---
-local MIN_INPUT = 0.05
-local countID = 1
+local folderKeyCode = Instance.new("Folder", ReplicatedStorage)
+folderKeyCode.Name = "KeyCode"
 
 local module = {}
 
----@param func any | "" | {any | {}}
----@param buttons Enum.KeyCode{}
----@param state Enum.UserInputState{}?
----@param createButton boolean?
+---Use this if button not use delta or position
+---@param buttons nil {Enum.KeyCode}
+---@param state nil | {Enum.UserInputState}
+---@param funct nil {any & {any}}
+---@param createButton nil  boolean?
 ---@return any
-function module.newBind(
-	func: any | "" | { any | {} },
-	buttons: { { Enum.KeyCode | Vector2 } } | { Enum.KeyCode },
-	state: { Enum.UserInputState }?,
+function module.newNormal(
+	buttons: { Enum.KeyCode },
+	state: nil | { Enum.UserInputState },
+	funct: any | { ["0"]: any, ["1"]: { any } | any },
 	createButton: boolean?
-): number
-	local _state = if state == nil then { Enum.UserInputState.Begin } else state :: { Enum.UserInputState }
-	local _createButton = if createButton == nil then false else createButton
+): string
+	state = if state == nil then { Enum.UserInputState.Begin } else state
+	createButton = if createButton == nil then false else createButton
 
-	local id = tostring(countID)
+	local id = HttpService:GenerateGUID()
 
-	local param = {}
-
-	for i, v in buttons :: { any } do
-		if type(v) == "table" then
-			param[v[1]] = v[2]
-			v[i] = v[1] :: Enum.KeyCode
-
-			local value = Instance.new("Vector3Value", folderKeyCode)
-			value.Name = id
-		end
-	end
-
-	local function newfunct(actionName: string, inputState: Enum.UserInputState, inputObject: InputObject): ()
-		if actionName ~= id or not table.find(_state, inputState) then
+	local function ButtonFunction(actionName: string, inputState: Enum.UserInputState, inputObject: InputObject): ()
+		if actionName ~= id or not table.find(state :: { Enum.UserInputState }, inputState) then
 			return
 		end
 
-		if param[inputObject.KeyCode] then
-			local p = inputObject.Position
-			p = Vector3.new(math.abs(p.X) < MIN_INPUT or p.X, math.abs(p.Y) < MIN_INPUT or p.Y, 0)
-			folderKeyCode[id].Value = p
-		elseif type(func) == "function" then
-			func()
-		elseif type(func) == "table" then
-			local tb = func :: { any | {} }
-			tb[1](if type(tb[2]) == "table" then table.unpack(tb[2]) else tb[2])
+		if type(funct) == "table" then
+			local f, t = table.unpack(funct)
+			if type(t) == "table" then
+				f(table.unpack(t))
+			else
+				f(t)
+			end
+		else
+			funct()
 		end
 	end
 
-	ContextActionService:BindAction(tostring(countID), newfunct, _createButton, table.unpack(buttons))
+	ContextActionService:BindAction(id, ButtonFunction, createButton, table.unpack(buttons))
+	return id
+end
 
-	countID += 1
+---Use this if button use delta or position
+---@param buttons Enum.KeyCode{}
+---@param state nil | {Enum.UserInputState}
+---@param funct any
+---@return any
+function module.newPosition(buttons: { Enum.KeyCode }, state: nil | { Enum.UserInputState }, funct: any | nil): string
+	state = if state == nil then { Enum.UserInputState.Begin } else state
+
+	local id = HttpService:GenerateGUID()
+
+	local f
+	local PostitionVector, DeltaVector
+	do
+		f = Instance.new("Folder", folderKeyCode)
+		f.Name = id
+		PostitionVector = Instance.new("Vector3Value", f)
+		PostitionVector.Name = "Position"
+		DeltaVector = Instance.new("Vector3Value", f)
+		DeltaVector.Name = "Delta"
+	end
+
+	local function ButtonFunction(actionName: string, inputState: Enum.UserInputState, inputObject: InputObject): ()
+		if actionName ~= id or not table.find(state :: { Enum.UserInputState }, inputState) then
+			return
+		end
+
+		local p = inputObject.Position
+		local d = inputObject.Delta
+
+		local vc3 = { Position = Vector3.new(p.X, p.Y, 0), Delta = d }
+
+		PostitionVector.Value = vc3.Position
+		DeltaVector.Value = vc3.Delta
+
+		if type(funct) ~= "nil" then
+			funct(vc3)
+		end
+	end
+
+	ContextActionService:BindAction(id, ButtonFunction, false, table.unpack(buttons))
 	return id
 end
 
 ---@param id string
 function module.UnbindAction(id: string): ()
 	ContextActionService:UnbindAction(id)
+
+	if folderKeyCode:FindFirstChild(id) then
+		folderKeyCode:FindFirstChild(id):Destroy()
+	end
 end
 
---Create meta ControlID
-ConnectionMeta.AddDisconnect("ControlID", {
-	__mewindex = function(self, i, v)
-		rawset(self, i, v)
-		warn(self, i, v)
-	end,
-}, function(v)
+ConnectionMeta.AddDisconnect("ControlID", nil, function(v)
 	module.UnbindAction(v)
 end)
 
